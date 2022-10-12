63c13fc8a37f22bc01e20fc32758472e452abf97
js-madive <js.lee@madive.co.kr>
2022-10-12 17:44:25 +0900

    수정이

---
+++ src/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Test() {
+    return (
+        <div class="main-box">
+            <h2>제목목목제목</h2>
+            <p>내용</p>
+            <button type="button">버튼태그</button>
+        </div>
+    );
+}
+
+export default Test;
